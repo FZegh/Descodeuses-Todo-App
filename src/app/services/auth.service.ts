e45849c3d3476c6a957692a937f5cd42eda5ee5f
FZegh <f.zeghdalou@gmail.com>
2025-08-20 09:53:47 +0200

    ajout fichier auth.service.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { User } from '../models/user.models';
+import { environment } from '../../environments/environment';
+//import { LoginComponent } from '../components/login/login.component'; // adapte ce chemin si besoin
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = environment.apiUrl  + '/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  
+
+  signup(userData: any): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/auth/sign-up`, userData);}
+
+  login(payload: any): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.apiUrl}`, payload).pipe(
+      tap(response => {
+      sessionStorage.setItem('authToken', response.token); // 💾 stocker le JWT pour les prochaines requêtes
+    })
+    );
+  }
+
+  getToken(): string | null {
+    return sessionStorage.getItem('authToken');  // recuperer le token stocké
+  }
+
+  getUtilisateurConnecte(): Observable<User> {
+  return this.http.get<User>(`${environment.apiUrl}/api/utilisateur/me`);
+}
+
+
+}
